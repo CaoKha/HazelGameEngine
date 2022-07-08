fbf4b2ac12312df9021bb2c9d62d311861eab780
Steve <snguyen@fff.fr>
2022-07-08 10:35:32 +0200

    add premake build system

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -4,7 +4,12 @@
 
 extern Hazel::Application* Hazel::CreateApplication();
 
-int main(int arg, char** argv) {
+int main(int arg, char** argv) 
+{
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 5;
+	HZ_INFO("Hello! Var={0}", a);
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
